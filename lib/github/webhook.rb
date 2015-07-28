1ca6965bc4a1829c815268bc82f00a84fa5a7926
yukpiz <yukpiz@gmail.com>
2015-07-28 12:07:25 +0900

    Updated.

--- lib/github/webhook.rb
+++ lib/github/webhook.rb
@@ -11,10 +11,9 @@ class Github::Webhook
         eventtype = headers["X-Github-Event"]
         sha1 = headers["X-Hub-Signature"]
 
-        hmac = OpenSSL.HMAC.hexdigest("sha1", "aiueo", @request.body)
+        hmac = OpenSSL::HMAC.hexdigest("sha1", "aiueo", @request.body)
 
         puts @request.body
-
         puts sha1
         puts hmac
 
