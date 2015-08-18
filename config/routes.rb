86d78574114817de636ed4f552c256cef40bb7b6
yukpiz <yukpiz@gmail.com>
2015-08-18 19:24:57 +0900

    Updated blog site.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
     root 'topic#first'
     get '/article/:id' => 'article#single'
     get '/topic/:page' => 'topic#pages'
+    get '/tags/:tag' => 'topic#tags'
     mount Deploy::API => "/"
     mount Import::API => "/"
     mount Convert::API => "/"
